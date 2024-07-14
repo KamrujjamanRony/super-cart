900e336e318553b7a7158fa9e561184e297cb0e2
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-07-14 20:41:54 +0600

    product cart 2 create

---
+++ src/app/components/Shared/product-card-2/product-card-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-card-2.component.html',
+  styleUrl: './product-card-2.component.css'
+})
+export class ProductCard2Component {
+
+}
