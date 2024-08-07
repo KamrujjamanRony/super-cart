2abead01bb5d9d5e54b924003f630072badb5286
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-08-07 18:05:15 +0600

    shop: filter by brandname done

--- src/app/Pages/main/shop/shop.component.ts
+++ src/app/Pages/main/shop/shop.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { BreadcrumbsComponent } from '../../../components/Shared/breadcrumbs/breadcrumbs.component';
-import { ShopSidebarComponent } from '../../../components/shop/shop-sidebar/shop-sidebar.component';
 import { ProductCardComponent } from '../../../components/Shared/product-card/product-card.component';
 import { DataService } from '../../../services/data.service';
 import { CommonModule } from '@angular/common';
@@ -9,7 +8,7 @@ import { ProductCard2Component } from '../../../components/Shared/product-card-2
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [BreadcrumbsComponent, ShopSidebarComponent, ProductCardComponent, CommonModule, ProductCard2Component],
+  imports: [BreadcrumbsComponent, ProductCardComponent, CommonModule, ProductCard2Component],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
@@ -19,6 +18,8 @@ export class ShopComponent {
   products: any;
   categories: any;
   brands: any;
+  brandNames: string[] = [];
+  categoryNames: string[] = [];
   prices: any;
   sizes: any;
   colors: any;
@@ -31,8 +32,8 @@ export class ShopComponent {
       this.categories = this.groupProductsByProperty(this.products, 'category');
       this.brands = this.groupProductsByProperty(this.products, 'brand');
       this.prices = this.groupProductsByProperty(this.products, 'offer_price');
-      this.sizes = this.groupProductsByProperty(this.products, 'size');
-      this.colors = this.groupProductsByProperty(this.products, 'color');
+      this.sizes = this.groupProductsByArrayProperty(this.products, 'sizes');
+      this.colors = this.groupProductsByArrayProperty(this.products, 'colors');
       console.log(this.categories);
       console.log(this.brands);
       console.log(this.prices);
@@ -63,6 +64,67 @@ export class ShopComponent {
       title: prop,
       quantity: propertyMap[prop]
     }));
+  }
+
+  groupProductsByArrayProperty(products: any[], property: string): any[] {
+    const propertyMap = products.reduce((acc, product) => {
+      const props = product[property];
+      if (Array.isArray(props)) {
+        props.forEach(prop => {
+          if (!acc[prop]) {
+            acc[prop] = 0;
+          }
+          acc[prop]++;
+        });
+      }
+      return acc;
+    }, {});
+
+    return Object.keys(propertyMap).map(prop => ({
+      title: prop,
+      quantity: propertyMap[prop]
+    }));
+  }
+
+  toggleCategoryName(category: string, event: any): void {
+    if (event.target.checked) {
+      this.categoryNames.push(category);
+    } else {
+      const index = this.categoryNames.indexOf(category);
+      if (index > -1) {
+        this.categoryNames.splice(index, 1);
+      }
+    }
+  }
+
+  toggleBrandName(brand: string, event: any): void {
+    if (event.target.checked) {
+      this.brandNames.push(brand);
+    } else {
+      const index = this.brandNames.indexOf(brand);
+      if (index > -1) {
+        this.brandNames.splice(index, 1);
+      }
+    }
+  }
+
+  byCategory(data: any): any {
+    if (!this.categoryNames || this.categoryNames.length === 0) {
+      return data; // If the categoryNames array is empty, return all data
+    }
+  
+    const selectedData = data.filter((product: any) => product && this.categoryNames.includes(product.category.toString()));
+    return selectedData;
+  }
+
+
+  byBrand(data: any): any {
+    if (!this.brandNames || this.brandNames.length === 0) {
+      return data; // If the brandNames array is empty, return all data
+    }
+  
+    const selectedData = data.filter((product: any) => product && this.brandNames.includes(product.brand.toString()));
+    return selectedData;
   }
 
 }
