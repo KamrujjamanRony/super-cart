8be067863f7736456026902afdbe10c40be214c4
KamrujjamanRony <kamrujjaman0228@gmail.com>
2025-08-15 11:33:10 +0600

    update 15-08-2025

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+// environment.prod.ts
+export const environment = {
+    production: true,
+    apiUrl: 'http://supersoft:81/api',
+    authUrl: 'http://supersoft:81/HWS',
+    siteId: 1,
+    apiKey: "AIzaSyAFJCGAbWU3TJmmo1BJBSnnlwUin68wYvo",
+    authDomain: "super-cart-80b68.firebaseapp.com",
+    projectId: "super-cart-80b68",
+    storageBucket: "super-cart-80b68.firebasestorage.app",
+    messagingSenderId: "193969857306",
+    appId: "1:193969857306:web:931f5f4d45f918140002a3"
+};
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { register as registerSwiperElements } from 'swiper/element/bundle';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
 
+if (environment.production) enableProdMode();
 registerSwiperElements();
 
 bootstrapApplication(AppComponent, appConfig)
